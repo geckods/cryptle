e3bcb00562b1e6031645e45d3994d3444e45ff47
Abhinav Ramachandran <abhinav.ramachandran@uber.com>
2022-06-04 12:35:05 +0530

    Payout Script Improvements

--- react/scripts/payoutResetInit.py
+++ react/scripts/payoutResetInit.py
@@ -2,30 +2,47 @@ from brownie import *
 import os
 import json
 
+
 def main():
+    print("Network:", network.show_active())
     # requires brownie account to have been created
-    if network.show_active()=='development':
+    if network.show_active() == 'development':
         # add these accounts to metamask by importing private key
         owner = accounts[0]
 
     elif network.show_active() == 'avax-test':
         # add these accounts to metamask by importing private key
+        print("Adding Private Key")
         accounts.add(os.environ['PRIMARY_TEST_ACCOUNT_PRIVATE_KEY'])
         owner = accounts[0]
 
+        print("Fetching JSON ABI")
+        print("current working directory", os.getcwd())
         with open(
-                "/home/geckods/Blockchain/cryptle/react/client/src/artifacts/contracts/WordleVRF.json",
+                "../client/src/artifacts/contracts/WordleVRF.json",
                 'r') as f:
             wordleAbi = json.load(f)['abi']
 
         wordle = Contract.from_abi("wordle", os.environ['CURRENT_WORDLE_ADDRESS'], wordleAbi, accounts[0])
 
-        if wordle.getPlayerCount() > 0:
-            wordle.payoutAndReset({'from':owner})
+        if wordle.currGameState() == 0:
+            print("initGame")
             wordle.initGame({'from': owner})
+        elif wordle.getPlayerCount() > 0:
+            print("payoutAndReset")
+            wordle.payoutAndReset({'from': owner})
+            print("initGame")
+            wordle.initGame({'from': owner})
+        else:
+            print("Game initialised and no players signed up. Abandoning reset!")
+
 
 #             CRONTAB:
 # PRIMARY_TEST_ACCOUNT_PRIVATE_KEY=0x...
 # CURRENT_WORDLE_ADDRESS=0x...
 #
 # 59 23 * * *  cd /home/geckods/Blockchain/cryptle/react && /home/geckods/Blockchain/cryptle/env/bin/brownie run /home/geckods/Blockchain/cryptle/react/scripts/payoutResetInit.py --network avax-test
+
+if __name__ == "__main__":
+    network.connect('avax-test')
+    main()
